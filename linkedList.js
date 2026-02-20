316f76c18092c6b5102635502e997631058acb4c
jose-g315 <120354063+jose-g315@users.noreply.github.com>
2026-02-20 00:47:31 -0600

    implement append, prepend, size, and toString methods

--- linkedList.js
+++ linkedList.js
@@ -1,6 +1,52 @@
-class LinkedList {
-  constructor(parameters) {}
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+  size() {
+    let size = 0;
+    let current = this.head;
+    while (current) {
+      size += 1;
+      current = current.next;
+    }
+    return size;
+  }
+  head() {}
+  tail() {}
+  at(index) {}
+  pop() {}
+  contains(value) {}
+  findIndex(value) {}
+  toString() {
+    let current = this.head;
+    let result = '';
+    while (current) {
+      result += current.value + '->';
+      current = current.next;
+    }
+    console.log(result + 'null');
+  }
 }
 class Node {
-  constructor(parameters) {}
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
---
+++ main.js
@@ -0,0 +1,11 @@
+import LinkedList from './linkedList.js';
+
+const list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+list.append(5);
+
+list.toString();
+console.log(list.size());
